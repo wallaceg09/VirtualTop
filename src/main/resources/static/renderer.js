cc947daefbf6c9c74f87f93474ebf1ad01bd7d17
Glen Wallace <wallaceglenw@gmail.com>
2016-10-04 22:24:49 -0500

    Almost pulling map data from the REST interface...

--- src/main/resources/static/renderer.js
+++ src/main/resources/static/renderer.js
@@ -1,22 +1,30 @@
-//var map;
-//var layer1;
-
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', {preload: preload, create:create, update: update});
+var map;
+
+// Async loading:
+// http://www.html5gamedevs.com/topic/7491-async-image-loader/
+// http://www.html5gamedevs.com/topic/9103-synchronous-loading-with-gameloadjson/
 
 function preload(){
-    game.load.spritesheet("test", "/test.png", 16, 16);
+    game.load.json('map_json', '/map/get?id=242eceff-3ce0-4e23-9183-a63de7cbf66e');
+    game.load.image('test', 'test.png');
 }
 
 function create() {
-    //map = game.add.tilemap();
-    //layer1 = map.create("layer1", 40, 30, 16, 16)
-    var layer1 = game.add.group();
-    for(var y = 0; y < 38; y++) {
-        for(var x = 0; x < 50; x++) {
-            game.add.tileSprite(x*16, y*16, 16, 16, 'test', 50, layer1);
-        }
+    var map_json = game.cache.getJSON('map_json');
+
+    map = game.add.tilemap(map_json.json); // FIXME: How do I use the json property within the map_json?
+
+    var imgMap = map_json.imageMap;
+
+    for(var key in imgMap) {
+        var val = imgMap[key];
+        console.log(key + ' ' + val);
+        map.addTilesetImage(key, val);
     }
 
+    map.createLayer('Floor');
+    map.createLayer('Wall');
 }
 
 function update() {
