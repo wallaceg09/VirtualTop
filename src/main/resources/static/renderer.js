82aff760ed2c268db438d88c67e634b142c66b1f
Glen Wallace <wallaceglenw@gmail.com>
2016-10-06 21:44:00 -0500

    Add character spritesheet

--- src/main/resources/static/renderer.js
+++ src/main/resources/static/renderer.js
@@ -7,11 +7,20 @@ var map;
 
 function preload(){
     game.load.json('map_json', '/map/get?id=242eceff-3ce0-4e23-9183-a63de7cbf66e');
-//    game.load.onFileComplete.add(jsonComplete, this);
     game.load.image('test', 'roguelikeSheet_transparent.png');
+    game.load.spritesheet('sprites', 'roguelikeChar_transparent.png', 16, 16, spacing=1);
 }
 
 function create() {
+    parseMap();
+    parseSprites();
+}
+
+function parseSprites() {
+    game.add.sprite(100, 100, 'sprites', 0);
+}
+
+function parseMap() {
     var map_json = game.cache.getJSON('map_json');
 
     var tilemap_json = map_json['json'];
@@ -31,13 +40,9 @@ function create() {
 
     var layers = tilemap_json.layers;
 
-//    debugger;
     for(var i = 0; i < layers.length; i++) {
         map.createLayer(layers[i].name);
     }
-
-//    map.createLayer('Floor');
-//    map.createLayer('Wall');
 }
 
 function update() {
